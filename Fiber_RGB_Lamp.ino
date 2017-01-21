8d5d39ac12db83fdbcb424532ff6529235d0de38
Hylke44 <Hylke44@users.noreply.github.com>
2017-01-21 16:45:04 +0100

    Increased led pwm from depth of 8 to 16

--- Fiber_RGB_Lamp.ino
+++ Fiber_RGB_Lamp.ino
@@ -9,15 +9,19 @@
 //#                 #
 //##############################################################################
 
+const uint8_t interrupt_time[] = {104, 43, 32, 28, 24, 21, 20};
+
 volatile uint8_t new_frame = 1;
 uint8_t counter = 0;
 
-struct {
+typedef struct {
   uint8_t r;
   uint8_t g;
   uint8_t b;
-}led[4] = { {.r=0, .g=6, .b=3}, {.r=2, .g=1, .b=6}, 
-            {.r=5, .g=3, .b=0}, {.r=7, .g=5, .b=1} };
+}led_t;
+
+volatile led_t led[4] = { {.r=0, .g=5, .b=10}, {.r=4, .g=9, .b=14}, 
+                          {.r=8, .g=13, .b=2}, {.r=12, .g=1, .b=6} };
 
 void setup() {
   // put your setup code here, to run once:
@@ -34,12 +38,12 @@ void setup() {
   
   cli();//stop interrupts
 
-  //set timer0 interrupt at 480Hz
+  //set timer0 interrupt
   TCCR0A = 0;// set entire TCCR0A register to 0
   TCCR0B = 0;// same for TCCR0B
   TCNT0  = 0;//initialize counter value to 0
-  // set compare match register for 480khz
-  OCR0A = 172;// = (16MHz) / (480*256) - 1 (must be <256)
+  // set compare match register
+  OCR0A = 68;
   // turn on CTC mode
   TCCR0A |= (1 << WGM01);
   // Set CS02 bits for 256 prescaler
@@ -62,14 +66,13 @@ void loop() {
 
       //Circle some colors
       for (i = 0; i < 4; i++) {
-        led[i].r++;
-        if (led[i].r == 8) led[i].r = 0;
+        led[0].r++;
+        if (led[i].r == 16) led[i].r = 0;
         led[i].g++;
-        if (led[i].g == 8) led[i].g = 0;
+        if (led[i].g == 16) led[i].g = 0;
         led[i].b++;
-        if (led[i].b == 8) led[i].b = 0;
+        if (led[i].b == 16) led[i].b = 0;
       }
-
       
     }
   }
@@ -81,7 +84,7 @@ ISR(TIMER0_COMPA_vect) {
   uint8_t out_b = 0, out_c = 0, out_d = 0;
   
   frame_counter++;
-  if (frame_counter == 7) {
+  if (frame_counter == 15) {
     new_frame = 1;
     frame_counter = 0;
   }
